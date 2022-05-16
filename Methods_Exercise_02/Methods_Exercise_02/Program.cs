a1f0459a9b6e2f6df4cb4464857299b14c09dac2
johndward01 <johndward01@gmail.com>
2022-05-15 21:43:01 -0500

    moved the project over here

---
+++ Methods_Exercise_02/Methods_Exercise_02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods_Exercise_02
+{
+    internal class Program
+    {
+        public static void SampleMethod()
+        {
+            // method logic 
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO: Call each of the methods below 
+
+            #region Printing out each field on a single line with spaces in-between
+
+            // Example 1:
+            foreach (var item in _list1)
+            {
+                Console.Write($"{item} ");
+            }
+            Utility.AddSpaces(2);
+
+            //Example 2:
+            foreach (var item in _arr1)
+            {
+                Console.Write($"{item} ");
+            }
+            Utility.AddSpaces(2);
+
+            // Example 3:
+            foreach (var item in _str1)
+            {
+                Console.Write(item);
+            }
+            Utility.AddSpaces(2);
+
+            #endregion
+
+            #region Three examples of calling the above SampleMethod()
+
+            // Example 1:
+            // Console.WriteLine(SampleMethod());
+
+            /* 
+            Example 2:
+            var answer = SampleMethod();
+            Console.WriteLine(answer);
+            */
+
+            // Example 3:
+            // SampleMethod();
+
+            #endregion
+            // Hint: The way you choose to call the method will depend upon that methods return type!
+
+        }
+
+        // TODO: Create a method that takes the collection below and iterates through it printing out each item to the console
+        static List<string> _list1 = Utility.RandomColorsList(1, 11);
+
+
+        // TODO: Create a method that takes the array below and prints out Found it! if there is the string "blue" inside of it
+        static string[] _arr1 = Utility.RandomColorsArray(1, 11);
+
+
+        // TODO: Create a method that removes all of the vowels from the string below
+        static string _str1 = Utility.RandomString(50);
+    }
+}
---
+++ Methods_Exercise_02/Methods_Exercise_02/Utility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise_02
+{
+    internal class Utility
+    {
+        public static Random rng = new Random();
+        public static List<string> Colors = new List<string>() { "dark red", "red", "light red", "dark orange", "orange", "light orange", "dark yellow", "yellow", "light yellow", "dark green", "green", "light green", "dark blue", "blue", "light blue", "dark indigo", "indigo", "light indigo", "dark violet", "violet", "light violet", };
+
+
+        public static string RandomString(int length) // Thank you StackOverflow
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rng.Next(s.Length)]).ToArray());
+        }
+        public static List<string> RandomColorsList(int minValue, int maxValue) // Values must be within range of Int.32
+        {
+            List<string> list = new List<string>();
+            for (int i = 0; i < rng.Next(minValue, maxValue); i++)
+            {
+                list.Add(Colors[i]);
+            }
+            return list;
+        }
+        public static string[] RandomColorsArray(int minValue, int maxValue)
+        {
+            string[] colors = RandomlySizedArray(minValue, maxValue);
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var color = Colors[rng.Next(0, Colors.Count)];
+                colors[i] = color;
+            }
+            return colors;
+        }
+
+        public static void AddSpaces(int numberOfSpaces)
+        {
+            while (numberOfSpaces > 0)
+            {
+                Console.WriteLine();
+                numberOfSpaces--;
+            }
+        }
+        public static string[] RandomlySizedArray(int minValue, int maxValue) // Values must be within range of Int.32
+        {
+            var size = rng.Next(minValue, maxValue);
+            return new string[size];
+        }
+    }
+}
